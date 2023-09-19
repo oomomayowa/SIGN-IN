667b6c1951f1412987b0464dbde80e7d6b7c0e05
olayode justina <olayodejustina272@gmail.com>
2023-09-19 14:25:30 +0100

    good

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { Routes,Route, Navigate } from 'react-router-dom'
 import SignUp from './Pages/SignUp'
 import SignIn from './Pages/SignIn'
 import Dashboard from './Pages/Dashboard'
-import home from './Pages/home'
+import Land from './Pages/Land'
+
+
 
 const App =()=> {
   let token =localStorage.getItem("token")
   return (
     <>
      <Routes >
-      <Route path='/' element={<home/>}/>
+      <Route path='/' element={<Land/>}/>
       <Route path='/dashboard' element={token?<Dashboard/>:<Navigate to ="/signIn"/>}/>
       <Route path='/signup' element={<SignUp/>}/>
       <Route path='/signIn' element={<SignIn/>}/>
